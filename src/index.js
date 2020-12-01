4caece6ae466e174c0597703f1fdbaaf4a462241
Николай Кокорев <gruffbarbecue@gmail.com>
2020-12-01 23:00:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import App from './App';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
